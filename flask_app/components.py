e680113e5b28f058a3c75ab6a92ad5a104c82c5c
Nathan Burnham <nburnham022@gmail.com>
2024-08-16 11:56:51 -0400

    Rename home and index to the components

--- flask_app/components.py
+++ flask_app/components.py
@@ -5,9 +5,9 @@ bp = Blueprint('components', __name__, url_prefix = "/component")
 
 @bp.route('/table')
 def table():
-    return render_template('index/table.html', data=sample_data)
+    return render_template('components/table.html', data=sample_data)
 
 @bp.route('/pagination')
 def pagination():
     max_pages: int = 8
-    return render_template('index/pagination.html', max_pages=max_pages)
+    return render_template('components/pagination.html', max_pages=max_pages)
